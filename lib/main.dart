48aba8a452d4ee88d08c7e1fef6e50e99f280eb4
Ghina Aramita <ghinaramita@gmail.com>
2022-03-24 15:36:23 +0700

    final project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'fooderlich_theme.dart';
+import 'home.dart';
+
 void main() {
   runApp(const Fooderlich());
 }
+
 class Fooderlich extends StatelessWidget {
-  const Fooderlich({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+  const Fooderlich ({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
+
     return MaterialApp(
       title: 'Fooderlich',
       theme: theme,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Fooderlich')),
-        body: const Center(
-          child: Text('Let\'s get cooking 🥘'),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
